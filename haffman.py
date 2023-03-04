2ce33f62d7903bef2c5544ae0ee2a75a6ca8cd74
mvysotskyi <vysotskyi.pn@ucu.edu.ua>
2023-03-04 22:43:21 +0200

    Class prototype

---
+++ haffman.py
@@ -0,0 +1,30 @@
+"""
+"""
+
+from math import log2
+
+class HaffmanCode:
+    """
+    """
+    def __init__(self, propabilities: tuple[float]) -> None:
+        ...
+
+    def _encode(self) -> None:
+        """
+        """
+        ...
+
+    def code(self) -> tuple[str]:
+        """
+        """
+        ...
+
+    def average_length(self) -> float:
+        """
+        """
+        ...
+
+    def entropy(self) -> float:
+        """
+        """
+        ...
