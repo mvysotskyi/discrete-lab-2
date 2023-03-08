3a2be43762fd04e705b858530fb40e4705d59e4e
mvysotskyi <vysotskyi.pn@ucu.edu.ua>
2023-03-08 22:24:22 +0200

    Structure changes

--- compressors/compressor.py
+++ compressors/compressor.py
@@ -3,8 +3,6 @@ Compressor base class.
 This module contains the base class for all compressors. It is not meant to be used directly.
 """
 
-from typing import Any
-
 class Compressor:
     """
     Base class for all compressors.
@@ -12,13 +10,13 @@ class Compressor:
     def __init__(self, verbose=False):
         self.verbose = verbose
 
-    def compress(self, content: list[Any]):
+    def compress(self, src: str, dest: str):
         """
         Compresses the content.
         """
         raise NotImplementedError('subclasses of Compressor must provide a compress() method')
 
-    def decompress(self, **kwargs):
+    def decompress(self, src: str, dest: str):
         """
         Decompresses the content.
         """
